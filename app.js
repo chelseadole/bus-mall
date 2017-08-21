08f09e1625cd423e07442a6718aaa35cb0296727
CHELSEA DOLE <chelseadole@gmail.com>
2017-08-21 13:01:52 -0700

    created NewImg constructor, making numberGen and createNamePath methods within.

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 'use strict';
 
-function newImg(imgName, path, timesShown, timesClicked, imgID) {
+imgNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+pathOptions = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.jpg', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
+
+timesShown = 0;
+timesClicked = 0;
+
+function NewImg(imgName, path, timesShown, timesClicked, imgID) {
   this.imgName = imgName;
   this.path = path;
   this.timesShown = timesShown;
   this.timesClicked = timesClicked;
   this.imgID = imgID;
+
+  numberGen = function() {
+
+  }
+
+  createNamePath = function() {
+
+  };
 
 }
