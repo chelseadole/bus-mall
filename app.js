0b3b3933e1da335dbe0fdda4af4306a7b4811fda
CHELSEA DOLE <chelseadole@gmail.com>
2017-08-21 12:47:53 -0700

    added base HTML, creating obj constructor newImg

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function newImg(imgName, path, timesShown, timesClicked, imgID) {
+  this.imgName = imgName;
+  this.path = path;
+  this.timesShown = timesShown;
+  this.timesClicked = timesClicked;
+  this.imgID = imgID;
+
+}
