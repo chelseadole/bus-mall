159368c9c2955b33170fd5acf0de7eb6e2c053dd
CHELSEA DOLE <chelseadole@gmail.com>
2017-08-21 14:18:55 -0700

    finishing for loop, changing DOM commands to use setAttribute

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
 'use strict';
 
-imgNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var imgNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-pathOptions = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.jpg', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
+var pathOptions = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.jpg', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
 
-timesShown = 0;
-timesClicked = 0;
-thisImgsArr = [];
-lastImgsArr = [];
+var timesShown = 0;
+var timesClicked = 0;
+var thisImgsArr = [];
+var lastImgsArr = [];
 
-function NewImg(imgName, path, timesShown, timesClicked, imgID) {
+function NewImg(imgName, path, timesShown, timesClicked) {
   this.imgName = imgName;
   this.path = path;
   this.timesShown = timesShown;
   this.timesClicked = timesClicked;
-  this.imgID = imgID;
 };
 
+newImgObj();
 
-var newImgObj = function() {
-  for (i = 0; i < 3; i++) {
-    var indexNum = (Math.floor(Math.random) * 19);
-    if (this.lastImgsArr.contains(indexNum) || this.thisImgsArr.contains(indexNum)) {
+function newImgObj() {
+  for (var i = 1; i <= 3; i++) {
+    var indexNum = (Math.floor(Math.random) * 18);
+    console.log(lastImgsArr);
+    if (lastImgsArr.includes(imgNames[indexNum]) || thisImgsArr.includes(imgNames[indexNum])) {
       i--;
     } else {
-        var thisImg = new NewImg(imgNames[indexNum], pathOptions[indexNum], 0, 0);
-        this.thisImgs.push(indexNum);
+      var thisImg = new NewImg(imgNames[indexNum], pathOptions[indexNum], 0, 0);
+      thisImgsArr.push(indexNum);
+      var linkedImg = document.getElementById('img' + i);
+      linkedImg.setAttribute('src', pathOptions[indexNum]);
     }
-
-  var thisImg = new NewImg(imgNames[indexNum], pathOptions[indexNum], 0, 0);
-    }
-
   }
-}
-while
-var iterInt = (Math.floor(Math.random) * 19);
-
-
-var createNamePath = function() {
-  for
+};
